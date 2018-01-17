427d8aa03cf453857881ac1947aa9a7803c44070
Path3tic <michaeljoelaldrich@gmail.com>
2018-01-17 14:27:11 -0500

    I made a struct, I'll use it next time.

--- Source/TaskList/Private/TaskList.cpp
+++ Source/TaskList/Private/TaskList.cpp
@@ -97,21 +97,29 @@ void FTaskListModule::AddMenuExtension(FMenuBuilder& Builder)
 
 FReply FTaskListModule::UpdateTaskList()
 {
-	
-	//UE_LOG(LogTemp, Warning, TEXT("We do a thing when we hit the buton."))
-	FString TaskPrefix = FString("Dragons");
-	
-	FAssetRegistryModule& AssetRegistryModule = FModuleManager::LoadModuleChecked<FAssetRegistryModule>("AssetRegistry");
-	TArray<FAssetData> AssetData;
 	//TODO only get blueprints in content folder, ignore all blueprints in engine folder. Futhermore, allow user to constrain their search to within a specific directory, default to content.
 	//TODO allow for many different prefixes, or "tokens" as VS calls them
 	//TODO visual studio allows for the creation of "shortcuts" basically things to showup in the task list iwthout being a comment
 	//TODO visual stuio allows for arbitrary white space before the token, should we care about that?
 	//TODO consider supporting comments in other graphs, like materials, sound cues, animation bps/graphs and others I'm sure I'm forgetting. Probably should allow users to select the classes they want to search.
-	AssetRegistryModule.Get().GetAssetsByClass(FName("Blueprint"), AssetData);
 	
-	//auto Stuff = AssetData.Num();
-	//UE_LOG(LogTemp, Warning, TEXT("Number of Results: %i"), Stuff)
+	//NextList
+	//Need to layout the UI, build a method to retun SBox, or Sbutton, or some other slate object when given a TTaskSearchResult
+	//	FPropertyEditorModule maybe can show a TMap of keys "ActualTokens" and values "Display Names"
+	//Need to Update list when SWindow::OnIsActiveChanged is called, need to figure out how to bind to that.
+	//In the mean time, we can keep using the button to do that, after formatting the UI.
+	/*
+	Replace updateTaskList with 2 method calls array of TTaskSearchResult ParseProjectForTaskList() and void RefreshTaskDisplay(array of TTaskSearchResults)
+	This will allow us to insert fake results if necessary(either for testing or for "found nothing" results.)
+	*/
+	//FKismetEditorUtilities::BringKismetToFocusAttentionOnObject
+	//UE_LOG(LogTemp, Warning, TEXT("We do a thing when we hit the buton."))
+	
+	FString TaskPrefix = FString("Dragons"); //TODO promote this to property of the class, transform into array, and figure out configuration
+	
+	FAssetRegistryModule& AssetRegistryModule = FModuleManager::LoadModuleChecked<FAssetRegistryModule>("AssetRegistry");
+	TArray<FAssetData> AssetData;
+	AssetRegistryModule.Get().GetAssetsByClass(FName("Blueprint"), AssetData);
 	
 	TArray<UEdGraphNode_Comment*> ResultCommentNodes;
 	
@@ -130,7 +138,8 @@ FReply FTaskListModule::UpdateTaskList()
 			{
 				if (ActiveCommentNode->GetNodeTitle(ENodeTitleType::FullTitle).ToString().StartsWith(TaskPrefix))
 				{
-					ResultCommentNodes.Add(ActiveCommentNode);
+					ResultCommentNodes.AddUnique(ActiveCommentNode);
+					//ResultCommentNodes.Add(ActiveCommentNode);
 				}
 			}
 		}
